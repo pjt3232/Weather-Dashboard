946021aea02e2e49101079c4ac880f0e7e163041
Tranel <tranel@Tranels-MacBook-Air.local>
2023-04-11 08:08:07 -0500

    Finished JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     function displayCurrentWeather(city) {
-        apiKey = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=";
+        apiKey = "f50566f98e836f619d28a18fec37bb50";
         queryUrl = "api.openweathermap.org/data/2.5/forecast?" + city + "&appid=" + apiKey + "&units=metric";
 
         $.ajax({
@@ -21,9 +21,9 @@ $(document).ready(function() {
             var weatherIconEl = $("<img>").addClass("weather-icon").attr("src", weatherIconUrl);
             currentWeatherCardBody.append(weatherIconEL);
  
-            var temperatureEl = $("<p>").addClass("card-text").html("<strong>Temperature: </strong>" + response.main.temp + " &deg;C");
-            var humidityEl = $("<p>").addClass("card-text").html("<strong>Humidity: </strong>" + response.main.humidity + "%");
-            var windSpeedEl = $("<p>").addClass("card-text").html("<strong>Wind Speed: </strong>" + response.wind.speed + " m/s");
+            var temperature = $("<p>").addClass("card-text").html("<strong>Temperature: </strong>" + response.main.temp + " &deg;C");
+            var humidity = $("<p>").addClass("card-text").html("<strong>Humidity: </strong>" + response.main.humidity + "%");
+            var windSpeed = $("<p>").addClass("card-text").html("<strong>Wind Speed: </strong>" + response.wind.speed + " m/s");
             currentWeatherCardBody.append(temperatureEl, humidityEl, windSpeedEl);
 
             currentWeatherCard.append(currentWeatherCardBody);
@@ -34,8 +34,8 @@ $(document).ready(function() {
         });
     }
     
-    function displayForecas(city) {
-        apiKey = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=";
+    function displayForecast(city) {
+        apiKey = "f50566f98e836f619d28a18fec37bb50";
         queryUrl = "api.openweathermap.org/data/2.5/forecast?" + city + "&appid=" + apiKey + "&units=metric";
 
         $.ajax({
@@ -46,8 +46,65 @@ $(document).ready(function() {
             var forecastCardDeck = $("<div>").addClass("card-deck forecast-card-deck");
 
             for(var i = 0; i <response.list.length; i++) {
-                
+                var forecastCard = $("<div>").addClass("card");
+                var forecastCardBody = $("<div>").addClass("card-body");
+                var date = new Date(response.list[i].dt * 1000).toLocaleDateString();
+                var heading = ("<h5>").addClass("card-title").text(date);
+
+                forecastCardBody.append(heading);
+
+                var iconUrl = "https://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png";
+                var iconImg = $("<img").addClass("weather-icon").attr("src", iconUrl);
+                forecastCardBody.append(iconImg);
+
+                var temperatureEl = $("<p>").addClass("card-text").html("<strong>Temp: </strong>" + response.lsit.main.temp + " &deg;C");
+                var humidityEl = $("<p>").addClass("card-text").html("<strong>Humidity: </strong>" + response.lsit.main.humidity + "%");
+                var windSpeedEl = $("<p>").addClass("card-text").html("<strong>Wind Speed: </strong>" + response.lsit.main.wind.speed + " m/s");
+
+                forecastCardBody.append(temperatureEl, humidityEl, windSpeedEl);
+                forecastCard.append(forecastCardBody);
+                forecastCardDeck.append(forecastCard);
             }
+            $("forecast").empty().append(forecastCardDeck);
+        });
+
+        function saveCityToLocalStorage(city) {
+            var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) ;
+            
+            if (searchHistory.indexOf(city) === -1) {
+                searchHistory.unshift(city);
+                if (searchHistory.length > 10) {
+                    searchHistory.pop();
+                }
+
+                localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+            }
+        }
+
+        function updateSearchHistory() {
+            var serchHistory = JSON.parse(localstorage.getItem("searchHistory"));
+            var searchHistoryList = $("<ul>").addCLass("list-group");
+
+            for(var i = 0; i < searchHistory.length; i++) {
+                var searchHistoryItem = $("<li>").addClass("list-group-item").text(searchHistory[i]);
+                searchHistoryItem.on("click", function() {
+                    var city = $(this).text();
+                    displayCurrentWeather(city);
+                    displayForecast(city);
+                });
+                searchHistoryList.append(searchHistoryItem);
+            }
+            $("search-history").empty().append(searchHistoryList);
+        }
+
+        $("search-form").submit(function(event){
+            event.preventDefault();
+
+            var city = $("city-input").val();
+            displayCurrentWeather();
+            displayForecast();
+
+            $("city-input").val("");
         })
     }
-})
+});
