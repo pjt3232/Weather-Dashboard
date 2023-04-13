20c51c2b2738ccb0181e739a9ed39172d4146389
Tranel <tranel@Tranels-MacBook-Air.local>
2023-04-13 10:19:53 -0500

    Updated JS and README

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Weather-Dashboard
+
+## Description
+My motivation for this project was to better my skills to hopefully prepare myself for a job in the field in the future. I built this project with the intention of building somethign using server side APIs. This application solves the need of knowing the weather and 5 day forecast in the city you are in or anywhere else. I learned how to use fetch functions for URLs, I learned how to manipulate a server side API URL to get various information, and I learned how to create a search history that is shown on the page and stored in local storage.
+
+## Installation
+To install this application go to the url "https://pjt3232.github.io/Weather-Dashboard/" on the browser of your choice.
+
+## Usage
+To use this application, go to the url "https://pjt3232.github.io/Weather-Dashboard/". The application should look like the screenshot below:
+
+![Screenshot of the deployed application](./assets/images/Weather-Dashboard-Screenshot.png)
+
+To use this application just type in a city name in the search bar that says "Enter a city" and click the search button. This will add the city to your search history which has a maximum amount of 10 cities at a time and then pull up the weather for the current day and the 5 day forecast. The current day and 5 day forecast shows an icon that generally describes the weather conditions, the temperature in ºF, the humidity in %, and the wind speed in MPH. The weather for the 5 day forecast will show the weather at this current time on the that specific day.
+
+## Credits
+I did not use a third party starter code for this project or have any collaborators.
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,33 @@ $(document).ready(function() {
 
     function displayCurrentWeather(city) {
         apiKey = "f50566f98e836f619d28a18fec37bb50";
-        queryUrl = "api.openweathermap.org/data/2.5/forecast?" + city + "&appid=" + apiKey + "&units=metric";
+        queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
 
-        $.ajax({
-            url: queryUrl,
-            method: "GET"
-        }) .then(function(response) {
+        fetch(queryUrl)
+
+        .then(function (response) {
+          if (!response.ok) {
+            throw response.json();
+          }
+         return response.json();
+        }).then(function(response) {
             console.log(response);
             var currentDateTime = dayjs().format("dddd, MMMM D, YYYY h:mm A");
             var currentWeatherCard = $("<div>").addClass("card current-weather-card");
             var currentWeatherCardBody = $("<div>").addClass("card-body");
-            var cityNameEl = $("<h2>").addClass("card-title").text(response.name);
+            var cityNameEl = $("<h2>").addClass("card-title").text(response.city.name);
             var currentDateTimeEl = $("<h3>").addClass("card-subtitle mb-2 text-muted").text(currentDateTime);   
             currentWeatherCardBody.append(cityNameEl, currentDateTimeEl);
 
-            var weatherIconCode = response.weather[0].icon;
-            var weatherIconUrl = "https://openweathermap.org/img/w/" + weatherIconCode + ".png";
+            var weatherIconCode = response.list[0].weather[0].icon;
+            var weatherIconUrl = "http://api.openweathermap.org/img/w/" + weatherIconCode + ".png";
             var weatherIconEl = $("<img>").addClass("weather-icon").attr("src", weatherIconUrl);
-            currentWeatherCardBody.append(weatherIconEL);
+            currentWeatherCardBody.append(weatherIconEl);
  
-            var temperature = $("<p>").addClass("card-text").html("<strong>Temperature: </strong>" + response.main.temp + " &deg;C");
-            var humidity = $("<p>").addClass("card-text").html("<strong>Humidity: </strong>" + response.main.humidity + "%");
-            var windSpeed = $("<p>").addClass("card-text").html("<strong>Wind Speed: </strong>" + response.wind.speed + " m/s");
-            currentWeatherCardBody.append(temperatureEl, humidityEl, windSpeedEl);
+            var temperature = $("<p>").addClass("card-text").html("<strong>Temperature: </strong>" + response.list[0].main.temp + " ºF");
+            var humidity = $("<p>").addClass("card-text").html("<strong>Humidity: </strong>" + response.list[0].main.humidity + "%");
+            var windSpeed = $("<p>").addClass("card-text").html("<strong>Wind Speed: </strong>" + response.list[0].wind.speed + " MPH");
+            currentWeatherCardBody.append(temperature, humidity, windSpeed);
 
             currentWeatherCard.append(currentWeatherCardBody);
             $("#current-weather").empty().append(currentWeatherCard);
@@ -36,40 +40,49 @@ $(document).ready(function() {
     
     function displayForecast(city) {
         apiKey = "f50566f98e836f619d28a18fec37bb50";
-        queryUrl = "api.openweathermap.org/data/2.5/forecast?" + city + "&appid=" + apiKey + "&units=metric";
+        queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
 
-        $.ajax({
-            url: queryUrl,
-            method: "GET"
-        }) .then(function(response) {
+        fetch(queryUrl)
+
+        .then(function (response) {
+          if (!response.ok) {
+            throw response.json();
+          }
+         return response.json();
+        }).then(function(response) {
             console.log(response);
+            forecastList = $("<div>").addClass("row row-grid w-100");
             var forecastCardDeck = $("<div>").addClass("card-deck forecast-card-deck");
 
-            for(var i = 0; i <response.list.length; i++) {
-                var forecastCard = $("<div>").addClass("card");
+            for(var i = 0; i < response.list.length; i+=8) {
+                var forecastCard = $("<div>").addClass("card mb-3");
                 var forecastCardBody = $("<div>").addClass("card-body");
                 var date = new Date(response.list[i].dt * 1000).toLocaleDateString();
-                var heading = ("<h5>").addClass("card-title").text(date);
+                var heading = $("<h5>").addClass("card-title").text(date);
 
                 forecastCardBody.append(heading);
 
-                var iconUrl = "https://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png";
-                var iconImg = $("<img").addClass("weather-icon").attr("src", iconUrl);
+                var iconCode = response.list[i].weather[0].icon;
+                var iconUrl = "http://api.openweathermap.org/img/w/" + iconCode + ".png";
+                var iconImg = $("<img>").addClass("weather-icon").attr("src", iconUrl);
                 forecastCardBody.append(iconImg);
 
-                var temperatureEl = $("<p>").addClass("card-text").html("<strong>Temp: </strong>" + response.lsit.main.temp + " &deg;C");
-                var humidityEl = $("<p>").addClass("card-text").html("<strong>Humidity: </strong>" + response.lsit.main.humidity + "%");
-                var windSpeedEl = $("<p>").addClass("card-text").html("<strong>Wind Speed: </strong>" + response.lsit.main.wind.speed + " m/s");
+                var temperatureEl = $("<p>").addClass("card-text").html("<strong>Temp: </strong>" + response.list[i].main.temp + " ºF");
+                var humidityEl = $("<p>").addClass("card-text").html("<strong>Humidity: </strong>" + response.list[i].main.humidity + "%");
+                var windSpeedEl = $("<p>").addClass("card-text").html("<strong>Wind Speed: </strong>" + response.list[i].wind.speed + " MPH");
 
                 forecastCardBody.append(temperatureEl, humidityEl, windSpeedEl);
+                forecastCard.addClass("col-12");
                 forecastCard.append(forecastCardBody);
                 forecastCardDeck.append(forecastCard);
+                forecastList.append(forecastCardDeck);
             }
-            $("forecast").empty().append(forecastCardDeck);
+            $("#forecast").empty().append(forecastList);
         });
+    }
 
         function saveCityToLocalStorage(city) {
-            var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) ;
+            var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
             
             if (searchHistory.indexOf(city) === -1) {
                 searchHistory.unshift(city);
@@ -77,13 +90,13 @@ $(document).ready(function() {
                     searchHistory.pop();
                 }
 
-                localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+                localStorage.setItem("search-history", JSON.stringify(searchHistory));
             }
         }
 
         function updateSearchHistory() {
-            var serchHistory = JSON.parse(localstorage.getItem("searchHistory"));
-            var searchHistoryList = $("<ul>").addCLass("list-group");
+            var searchHistory = JSON.parse(localStorage.getItem("search-history")) || [];
+            var searchHistoryList = $("<ul>").addClass("list-group");
 
             for(var i = 0; i < searchHistory.length; i++) {
                 var searchHistoryItem = $("<li>").addClass("list-group-item").text(searchHistory[i]);
@@ -94,17 +107,18 @@ $(document).ready(function() {
                 });
                 searchHistoryList.append(searchHistoryItem);
             }
-            $("search-history").empty().append(searchHistoryList);
+            $("#search-history").empty().append(searchHistoryList);
         }
 
-        $("search-form").submit(function(event){
+        $(".btn-primary").on("click", function(event) {
             event.preventDefault();
+            console.log(event)
 
-            var city = $("city-input").val();
-            displayCurrentWeather();
-            displayForecast();
+            var city = $("#city-input").val();
+            console.log(city)
+            displayCurrentWeather(city);
+            displayForecast(city);
 
-            $("city-input").val("");
+            $("#city-input").val("");
         })
-    }
 });
